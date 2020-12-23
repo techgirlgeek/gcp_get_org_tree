c24993798fef94566e0ad61e93846391f62151c0
Karyn Cassio <karyn.cassio@davita.com>
2020-12-23 15:17:57 -0700

    Move getting projects to a function.

--- list_org_folders.sh
+++ list_org_folders.sh
@@ -1,13 +1,59 @@
 #!/usr/bin/env bash
 
-# setting gcloud formats for script
+# setting CSV gcloud format for script
 FORMAT="csv[no-heading](name,displayName.encode(base64))"
-FORMAT_PRJ="table[box,title='Folder ${NAME} Project List'] \
-(createTime:sort=1,name,projectNumber,projectId:label=ProjectID,parent.id:label=Parent)"
 
 printf "Are you parsing the full organization or just folders?\n"
 read -p "Enter ORG or FLD: "
 
+# Function to enumerate Folders recursively
+folders() {
+  LINES=("$@")
+  for LINE in ${LINES[@]}
+  do
+    # Parses lines of the form folder,name
+    VALUES=(${LINE//,/ })
+    FOLDER=${VALUES[0]}
+
+    # Decodes the encoded name
+    NAME=$(echo ${VALUES[1]} | base64 --decode)
+    printf "Folder: ${FOLDER} (${NAME})\n\n"
+
+    printf "Project: Project info:\n\n"
+    # PROJECT=$(gcloud projects list \
+    #   --filter parent.id:${FOLDER} \
+    #   --format="${FORMAT_PRJ}")
+    projects ${FOLDER}
+
+    if [ -z "$PROJECT" ]; then
+      printf "Folder: ${FOLDER} - ${NAME} has no sub-projects\n\n"
+    else
+      printf "Parent FolderID: ${FOLDER}\t Parent Name(s): ${NAME}\n${PROJECT} \n\n"
+    fi
+
+    folders $(gcloud resource-manager folders list \
+      --folder=${FOLDER} \
+      --format="${FORMAT}")
+
+  done
+}
+
+# Function to get Folder's Projects
+projects() {
+  # Format Project information into table
+  FORMAT_PRJ="table[box,title='Folder ${NAME} Project List'] \
+  (createTime:sort=1,name,projectNumber,projectId:label=ProjectID,parent.id:label=Parent)"
+
+  PROJECT=$(gcloud projects list \
+    --filter parent.id:${FOLDER} \
+    --format="${FORMAT_PRJ}")
+
+}
+
+# Request CLI input
+# Parse depending on input
+# ORG gives all folders and projects
+# FLD starts at a specific parent folder
 if [ ${REPLY} == "ORG" ]; then
   # Start at the Org
   if [ -z "${ORGANIZATION}" ]; then
@@ -23,45 +69,15 @@ else
   printf "Folder: ${FOLDER}\n\n"
   PARSEOPT="folder=${FOLDER}"
 
-  PROJECT=$(gcloud projects list \
-        --filter parent.id:"${FOLDER}" \
-        --format="${FORMAT_PRJ}")
+  projects ${FOLDER}
+  # PROJECT=$(gcloud projects list \
+  #       --filter parent.id:"${FOLDER}" \
+  #       --format="${FORMAT_PRJ}")
 
   printf "Folder: ${FOLDER}\n"
       printf "Project: Project info:\n\n"
       printf "${PROJECT}\n\n"
 fi
-
-# Enumerates Folders recursively
-folders() {
-  LINES=("$@")
-  for LINE in ${LINES[@]}
-  do
-    # Parses lines of the form folder,name
-    VALUES=(${LINE//,/ })
-    FOLDER=${VALUES[0]}
-
-    # Decodes the encoded name
-    NAME=$(echo ${VALUES[1]} | base64 --decode)
-    printf "Folder: ${FOLDER} (${NAME})\n\n"
-
-    printf "Project: Project info:\n\n"
-    PROJECT=$(gcloud projects list \
-      --filter parent.id:${FOLDER} \
-      --format="${FORMAT_PRJ}")
-
-    if [ -z "$PROJECT" ]; then
-      printf "Folder: ${FOLDER} - ${NAME} has no sub-projects\n\n"
-    else
-      printf "Parent FolderID: ${FOLDER}\t Parent Name(s): ${NAME}\n${PROJECT} \n\n"
-    fi
-
-    folders $(gcloud resource-manager folders list \
-      --folder=${FOLDER} \
-      --format="${FORMAT}")
-
-  done
-}
 
 LINES=$(gcloud resource-manager folders list \
   --"${PARSEOPT}" \
